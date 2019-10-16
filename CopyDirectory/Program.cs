1b9b339ae619fbe2ce652b522a15a70096d2385b
unknown <tarekakkawi89@gmail.com>
2019-10-16 15:03:52 +0300

    First Commit

---
+++ CopyDirectory/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CopyDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            // I felt that retrieving source path and inserting a target path was flexible.
+            // Therefore i decided to let the user copy paste source path and insert target
+            Console.WriteLine("Welcome to File&Folder Copying feauture");
+            Console.WriteLine();
+            Console.WriteLine("In order to initiate we require two important things from you:" + Environment.NewLine + "1)Source path (Just copy paste it)");
+            string SourcePath =   Console.ReadLine();
+            SourcePath.Replace(@"\",@"\\");
+            Console.WriteLine("2) Target path (Just copy paste it)");
+            string Targetpath = Console.ReadLine();
+            Targetpath.Replace(@"\", @"\\");
+
+            // retrieving files from source path adn target 
+            Dictionary<string,string> sourcefiles =  CopyDirectory_Logic.DirectoryFiles.SeperatingFilesFromFolders(SourcePath,true);
+            
+            if (sourcefiles.Count == 0)
+            {
+                Console.WriteLine("You have inserted a false path. The program will now exit");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            int Numbering = 0;
+            Console.WriteLine();
+            foreach (KeyValuePair<string,string> i in sourcefiles)
+            {
+              
+                Console.WriteLine(Numbering + " " + i.Key + " " + i.Value);
+                Numbering += 1;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Do you wish to use the copy functionality Y / N");
+            string CopyYorN = Console.ReadLine();
+            if(CopyYorN == "Y" || CopyYorN == "y")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Chose File or Folder to Copy");
+                string FileorFolder = Console.ReadLine();
+                if (FileorFolder == "File" || FileorFolder == "file")
+                {
+                    Numbering = 0;
+                    Dictionary<string, string> onlyfiles = CopyDirectory_Logic.DirectoryFiles.UserSelectedFiles();
+                    Console.WriteLine();
+                    foreach (KeyValuePair<string, string> i in onlyfiles)
+                    {
+                        
+                        
+                        Console.WriteLine(Numbering + " " + i.Key + " " + i.Value);
+                        Numbering += 1;
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Choose file you want to copy by typing number");
+                    string FileNumber =  Console.ReadLine();
+                    int value;
+                    if(int.TryParse(FileNumber,out value))
+                    {
+                        if (value > Numbering || value < 0)
+                        {
+                            Console.WriteLine("No file with such number");
+                        }
+                        else
+                        {
+                            bool IfFileCopied = CopyDirectory_Logic.DirectoryFiles.UserSelectsFileToCopy(value, onlyfiles, Targetpath);
+                            if(IfFileCopied)
+                            {
+                                Console.WriteLine("The file has been succesfully copied");
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("The file has not been copied as the source path is the same as the target path");
+                            }
+                            
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Type in numeric format");
+                    }
+                    
+
+                }
+                else if (FileorFolder == "Folder" || FileorFolder == "folder")
+                {
+                    Numbering = 0;
+                    Dictionary<string, string> onlyfolders = CopyDirectory_Logic.DirectoryFiles.UserSelectedFolder();
+                    Console.WriteLine();
+                    foreach (KeyValuePair<string, string> i in onlyfolders)
+                    {
+                        
+
+                        Console.WriteLine(Numbering + " " + i.Key + " " + i.Value);
+                        Numbering += 1;
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Choose folder you want to copy by typing number");
+                    string FolderNumber = Console.ReadLine();
+                    int value;
+                    if (int.TryParse(FolderNumber, out value))
+                    {
+                        if (value > Numbering || value < 0)
+                        {
+                            Console.WriteLine("No folder with such number");
+                        }
+                        else
+                        {
+                            bool IfFolderCopied = CopyDirectory_Logic.DirectoryFiles.UserSelectsFolderToCopy(value, onlyfolders, Targetpath);
+                            if (IfFolderCopied)
+                            {
+                                Console.WriteLine("The folder has been succesfully copied");
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("The folder has not been copied");
+                            }
+
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Type in numeric format");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please choose File or Folder");
+                }
+            }
+            else
+            {
+                Console.Clear();
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CopyDirectory_Logic/DirectoryFiles.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace CopyDirectory_Logic
+{
+    public static class DirectoryFiles
+    {
+       
+
+
+        static Dictionary<string, string> FileFolder = new Dictionary<string, string>();
+        static Dictionary<string, string> OnlyFiles = new Dictionary<string, string>();
+        static Dictionary<string, string> OnlyFolder = new Dictionary<string, string>();
+        static Dictionary<string, string> Subfolder = new Dictionary<string, string>();
+
+
+
+
+
+        // List all folders and files of directory
+        private static string[]  GetDirectoryandFiles(string _path, bool IsSourcePath) //DirectoryEnum _directoryenum)
+        {
+            string[] filefolderpath = new string[] { };
+            try
+            {
+                
+                switch (IsSourcePath)
+                {
+                    case true:
+                        {
+                            //Get all files and folder in path
+                            filefolderpath = Directory.GetFileSystemEntries(_path);
+                            break;
+                        }
+
+
+                    case false:
+                        {
+                            filefolderpath = Directory.GetFileSystemEntries(_path);
+
+                            break;
+                        }
+
+                }
+               
+            }
+            catch(Exception ex)
+            {
+                var a = ex.Message;
+            }
+
+            return filefolderpath;
+
+        }
+
+        /*Retrieve files extension in order to seperate the two.
+          This would allow user to quickly identify files from folder in Console app */
+        private static List<string> RetrievingExtension(string _path, bool IsSourcePath)
+        {
+            List<string> fileExtension = new List<string>();
+            foreach(var i in GetDirectoryandFiles(_path, IsSourcePath))
+            {
+                string extension = Path.GetExtension(i);
+                if (extension.Equals(string.Empty))
+                {
+                    fileExtension.Add("Folder");
+                    
+                }
+                else
+                {
+                    fileExtension.Add("File");
+                }
+            }
+            return fileExtension;
+        }
+
+        //Seperating Files from Folders
+        public static Dictionary<string,string> SeperatingFilesFromFolders(string _path, bool IsSourcePath)
+        {
+            
+            for (int i = 0; i < GetDirectoryandFiles(_path, IsSourcePath).Length; i++)
+            {
+
+                FileFolder.Add(GetDirectoryandFiles(_path, IsSourcePath)[i].ToString(), RetrievingExtension(_path, IsSourcePath)[i].ToString());
+            }
+          
+            return FileFolder;
+        }
+
+        //When user selectsfiles to copy, it will display only files in the DIR
+        public static Dictionary<string,string> UserSelectedFiles()
+        {
+             foreach(var i in FileFolder)
+            {
+                if(i.Value =="File")
+                {
+                    OnlyFiles.Add(i.Key, i.Value);
+                }
+            }
+            return OnlyFiles;
+        }
+        
+        //The selected file will be copied
+        public static bool UserSelectsFileToCopy(int filenumber, Dictionary<string,string> onlyfiles, string Targetpath)
+        {
+            bool FileCopied = false;
+            // User selects based on numbering
+            string filetobecopied = onlyfiles.ElementAt(filenumber).Key;
+            List<string> retrieveFileName = filetobecopied.Split("\\").ToList();
+            string newlocation = Targetpath + "\\" + retrieveFileName.LastOrDefault();
+
+
+            //checks if file has been copied.
+            if (!File.Exists(newlocation))
+            {   //copy only for other location
+                
+                File.Copy(filetobecopied, newlocation);
+                FileCopied = true;
+            }
+
+            
+            return FileCopied;
+        }
+
+
+        //When user selectsfiles to copy, it will display only files in the DIR
+        public static Dictionary<string, string> UserSelectedFolder()
+        {
+            foreach (var i in FileFolder)
+            {
+                if (i.Value == "Folder" +
+                    "")
+                {
+                    OnlyFiles.Add(i.Key, i.Value);
+                }
+            }
+            return OnlyFiles;
+        }
+
+        //The selected folder will be copied with all files in it
+        public static bool UserSelectsFolderToCopy(int foldernumber, Dictionary<string,string> onlyfolders, string Targetpath)
+        {
+            bool Copiedsuccess = false;
+            bool FilesCopied = false;
+            bool SubFoldersCopied = false;
+
+            string foldertobecopied = onlyfolders.ElementAt(foldernumber).Key;
+            var splitfolderpath = foldertobecopied.Split("\\");
+            //create target
+            var target = Directory.CreateDirectory(Targetpath + "\\" + splitfolderpath.LastOrDefault());
+            //Get folder user picked
+          
+            //Get all files in folder
+            var a = new DirectoryInfo(foldertobecopied);
+            //copying files to target dir recursively
+            for(int i=0; i<=a.GetFiles().Length-1; i++)
+            {
+                OnlyFolder.Add(a.GetFiles().ElementAt(i).FullName, a.GetFiles().ElementAt(i).Extension);
+                UserSelectsFileToCopy(i, OnlyFolder, target.ToString());
+                
+                FilesCopied = true;
+            }
+
+            
+
+            //Copying subfolder
+            for(int i=0; i<=a.GetDirectories().Length-1; i++)
+            {
+                Subfolder.Clear();
+                Subfolder.Add(a.GetDirectories().ElementAt(i).FullName,"");
+                UserSelectsFolderToCopy(i, Subfolder, target.ToString());
+                SubFoldersCopied = true;
+            }
+
+
+            if(FilesCopied && SubFoldersCopied)
+            {
+                Copiedsuccess = true;
+            }
+
+            return Copiedsuccess;
+        }
+
+    }
+}
